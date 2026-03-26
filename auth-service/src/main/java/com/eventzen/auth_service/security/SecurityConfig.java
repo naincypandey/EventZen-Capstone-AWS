96f53ad79835dcbca808dc32726c8e3e9d2e9435
Naincy <naincy.pandey2003@gmail.com>
2026-03-26 22:01:08 +0530

    spring auto password conflict

--- auth-service/src/main/java/com/eventzen/auth_service/security/SecurityConfig.java
+++ auth-service/src/main/java/com/eventzen/auth_service/security/SecurityConfig.java
@@ -2,13 +2,15 @@ package com.eventzen.auth_service.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
+@EnableWebSecurity  // 🔥 ADD THIS
 public class SecurityConfig {
 
     @Bean
