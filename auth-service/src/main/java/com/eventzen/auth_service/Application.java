e8245ac3d9173ee3eb5145bb2e1893de375c0517
Naincy <naincy.pandey2003@gmail.com>
2026-03-26 19:34:33 +0530

    spring auto password conflict

--- auth-service/src/main/java/com/eventzen/auth_service/Application.java
+++ auth-service/src/main/java/com/eventzen/auth_service/Application.java
@@ -2,8 +2,10 @@ package com.eventzen.auth_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.eventzen.auth_service") // Force scan all sub-packages
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
